b2c7b6f846d2e12e431c3144af09dca0bb960914
Francesco <fr.ozzimo@gmail.com>
2016-09-20 21:25:42 +0200

    Made window customizable from the caller side

--- example.js
+++ example.js
@@ -15,6 +15,14 @@ console.log("Verifying Valid Token");
 console.log(twoFactor.verifyToken(newSecret.secret, newToken.token));
 
 console.log("*******************************");
+console.log("Verifying Valid Token - Window: 1");
+console.log(twoFactor.verifyToken(newSecret.secret, newToken.token, 1));
+
+console.log("*******************************");
+console.log("Verifying Valid Token - Window: -3");
+console.log(twoFactor.verifyToken(newSecret.secret, newToken.token, -3));
+
+console.log("*******************************");
 console.log("Verifying Invalid Token");
 console.log(twoFactor.verifyToken(newSecret.secret, '11111'));
 
