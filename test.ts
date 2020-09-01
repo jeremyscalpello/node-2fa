7a8d010c445ac22a572fd7b4be73a59a2e97ec69
Ariel Costas <ariel@arielcostas.es>
2020-09-01 23:37:39 +0200

    Rename example to test, and implement TS

--- test.ts
+++ test.ts
@@ -1,8 +1,11 @@
-var twoFactor = require('./index.js');
+import * as twoFactor from "./index";
 
 console.log("*******************************");
 console.log("Generating New Secret");
-var newSecret = twoFactor.generateSecret({name: 'My Awesome App', account: 'johndoe'});
+var newSecret = twoFactor.generateSecret({
+  name: "My Awesome App",
+  account: "johndoe",
+});
 console.log(newSecret);
 
 console.log("*******************************");
@@ -12,19 +15,35 @@ console.log(newToken);
 
 console.log("*******************************");
 console.log("Verifying Valid Token");
-console.log(twoFactor.verifyToken(newSecret.secret, newToken.token));
+console.log(
+  twoFactor.verifyToken({ secret: newSecret.secret, token: newToken!.token })
+);
 
 console.log("*******************************");
 console.log("Verifying Valid Token - Window: 1");
-console.log(twoFactor.verifyToken(newSecret.secret, newToken.token, 1));
+console.log(
+  twoFactor.verifyToken({
+    secret: newSecret.secret,
+    token: newToken!.token,
+    window: 1,
+  })
+);
 
 console.log("*******************************");
 console.log("Verifying Valid Token - Window: -3");
-console.log(twoFactor.verifyToken(newSecret.secret, newToken.token, -3));
+console.log(
+  twoFactor.verifyToken({
+    secret: newSecret.secret,
+    token: newToken!.token as string,
+    window: -3,
+  })
+);
 
 console.log("*******************************");
 console.log("Verifying Invalid Token");
-console.log(twoFactor.verifyToken(newSecret.secret, '11111'));
+console.log(
+  twoFactor.verifyToken({ secret: newSecret.secret, token: "11111" })
+);
 
 console.log("*******************************");
 console.log("Done - Star Me On Github");
