812a2493d601542394bab7139639bcbddac45fd1
Alistair Smith <alistairsmith01@gmail.com>
2021-01-11 09:07:23 +0000

    fix: use describe to share secret between tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,48 +1,34 @@
 import { generateSecret, generateToken, verifyToken } from "../src";
 
-test("Generates a valid secret without options", () => {
-  expect(typeof generateSecret().secret).toBe("string");
-});
-
-test("Generates a valid token", () => {
+describe("node-2fa", () => {
   const { secret } = generateSecret();
-  const result = generateToken(secret);
-  expect(typeof result?.token).toBe("string");
-});
+  const token = generateToken(secret);
 
-test("Verifies a valid token", () => {
-  const { secret } = generateSecret();
+  test("Generates a valid secret without options", () => {
+    expect(typeof secret).toBe("string");
+  });
 
-  const result = generateToken(secret);
-  expect(typeof result?.token).toBe("string");
+  test("Generates a valid token", () => {
+    expect(typeof token?.token).toBe("string");
+  });
 
-  const verification = verifyToken(secret, result?.token);
-  expect(typeof verification?.delta).toBe("number");
-});
+  test("Verifies a valid token", () => {
+    const verification = verifyToken(secret, token?.token);
+    expect(typeof verification?.delta).toBe("number");
+  });
 
-test("Verifies a token with window 1", () => {
-  const { secret } = generateSecret();
+  test("Verifies a token with window 1", () => {
+    const verification = verifyToken(secret, token?.token, 1);
+    expect(typeof verification?.delta).toBe("number");
+  });
 
-  const result = generateToken(secret);
-  expect(typeof result?.token).toBe("string");
+  test("Verifies a token with window -3", () => {
+    const verification = verifyToken(secret, token?.token, -3);
+    expect(verification).toBeNull();
+  });
 
-  const verification = verifyToken(secret, result?.token, 1);
-  expect(typeof verification?.delta).toBe("number");
-});
-
-test("Verifies a token with window -3", () => {
-  const { secret } = generateSecret();
-
-  const result = generateToken(secret);
-  expect(typeof result?.token).toBe("string");
-
-  const verification = verifyToken(secret, result?.token, -3);
-  expect(verification).toBeFalsy();
-});
-
-test("Check an invalid token", () => {
-  const { secret } = generateSecret();
-
-  const verification = verifyToken(secret, "111111");
-  expect(verification).toBeFalsy();
+  test("Check an invalid token", () => {
+    const verification = verifyToken(secret, "111111");
+    expect(verification).toBeNull();
+  });
 });
